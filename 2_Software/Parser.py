48ca8f32a998f938790466a87e6d04f29f4dc7f9
Justin <jsiekmann@email.arizona.edu>
2018-04-23 20:24:52 -0700

    comments

--- 2_Software/Parser.py
+++ 2_Software/Parser.py
@@ -5,40 +5,42 @@ import talib as ta
 import pickle
 import matplotlib.pyplot as plt
 quandl.ApiConfig.api_key = 'HwQoB4ePcDi8bFzJ6SJA'
-#%%
-#start_date = '2006-12-31'
-#end_date = '20017-12-31'
-#database_code = 'WIKI'
-#
-##tickers_financials = []
-##tickers_utilities = []
-##tickers_energy = []
-##tickers_healthcare = []
-##tickers_technology = []
-##tickers_realestate = []
-#
-##THIS TAKES ABOUT A MINUTE WITH THE CURRENT DATES
-#tickers_financials = ['JPM', 'BAC', 'WFC', 'C', 'MS']
-#tickers_utilities = ['T', 'VZ', 'NEE', 'TMUS']
-#tickers_energy = ['XOM', 'CVX', 'BP', 'GE', 'SLB']
-#tickers_healthcare = ['JNJ', 'UNH', 'PFE', 'MRK', 'ABBV', 'MMM', 'AMGN', 'MDT']
-#tickers_technology = ['AAPL', 'GOOGL', 'MSFT', 'FB', 'INTC', 'CSCO', 'ORCL', 'IBM', 'NVDA']
-#tickers_realestate = ['ECL', 'DWDP', 'FMC', 'IP', 'PPG', 'VMC', 'BMS']
-#tickers = [tickers_financials, tickers_utilities, tickers_energy, tickers_healthcare, tickers_technology, tickers_realestate]
-#    
-#data = {}
-#
-##data = quandl.get('WIKI/' + tickers, start_date=start_date, end_date=end_date)
-#
-#def import_data(ticker, start_date, end_date):
-#    return quandl.get('WIKI/' + ticker, start_date=start_date, end_date=end_date)
-#
-#for index in tickers:
-#    for ticker in index:
-#        print(ticker)
-#        data[ticker] = import_data(ticker, start_date, end_date)
-#        
-#    pickle.dump(data, open('data.pickle', 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+# =============================================================================
+# %% Generate Pickle %##
+# start_date = '2006-12-31'
+# end_date = '20017-12-31'
+# database_code = 'WIKI'
+# 
+# #tickers_financials = []
+# #tickers_utilities = []
+# #tickers_energy = []
+# #tickers_healthcare = []
+# #tickers_technology = []
+# #tickers_realestate = []
+# 
+# #THIS TAKES ABOUT A MINUTE WITH THE CURRENT DATES
+# tickers_financials = ['JPM', 'BAC', 'WFC', 'C', 'MS']
+# tickers_utilities = ['T', 'VZ', 'NEE', 'TMUS']
+# tickers_energy = ['XOM', 'CVX', 'BP', 'GE', 'SLB']
+# tickers_healthcare = ['JNJ', 'UNH', 'PFE', 'MRK', 'ABBV', 'MMM', 'AMGN', 'MDT']
+# tickers_technology = ['AAPL', 'GOOGL', 'MSFT', 'FB', 'INTC', 'CSCO', 'ORCL', 'IBM', 'NVDA']
+# tickers_realestate = ['ECL', 'DWDP', 'FMC', 'IP', 'PPG', 'VMC', 'BMS']
+# tickers = [tickers_financials, tickers_utilities, tickers_energy, tickers_healthcare, tickers_technology, tickers_realestate]
+#     
+# data = {}
+# 
+# #data = quandl.get('WIKI/' + tickers, start_date=start_date, end_date=end_date)
+# 
+# def import_data(ticker, start_date, end_date):
+#     return quandl.get('WIKI/' + ticker, start_date=start_date, end_date=end_date)
+# 
+# for index in tickers:
+#     for ticker in index:
+#         print(ticker)
+#         data[ticker] = import_data(ticker, start_date, end_date)
+#         
+#     pickle.dump(data, open('data.pickle', 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+# =============================================================================
 #%%
 data = pickle.load(open('data.pickle', 'rb'))
 #%%REFORMAT DATA 
@@ -49,28 +51,32 @@ for ticker in data:
 #%%
 
 data_new   = {}
-data_open  =  {}
-data_high  =   {}
-data_low   =    {}
-data_close =     {}
+data_open  = {}
+data_high  = {}
+data_low   = {}
+data_close = {}
 
-#  data_new[ticker]              = ta.SMA(data[ticker].iloc[:,0], timeperiod=20).to_frame()
-#  data_new[ticker]['SMA_High' ] = ta.SMA(data[ticker].iloc[:,1], timeperiod=20)
-#  data_new[ticker]['SMA_Low'  ] = ta.SMA(data[ticker].iloc[:,2], timeperiod=20)
-#  data_new[ticker]['SMA_Close'] = ta.SMA(data[ticker].iloc[:,3], timeperiod=20)
-#  data_new[ticker]['RSI_Open' ] = ta.RSI(data[ticker].iloc[:,0], timeperiod=14)
-#  data_new[ticker]['RSI_High' ] = ta.RSI(data[ticker].iloc[:,1], timeperiod=14)
-#  data_new[ticker]['RSI_Low'  ] = ta.RSI(data[ticker].iloc[:,2], timeperiod=14)
-#  data_new[ticker]['RSI_Close'] = ta.RSI(data[ticker].iloc[:,3], timeperiod=14)
-#  data_new[ticker]['OBV_Open' ] = ta.OBV(data[ticker].iloc[:,0], data[ticker].iloc[:,4])
-#  data_new[ticker]['OBV_High' ] = ta.OBV(data[ticker].iloc[:,1], data[ticker].iloc[:,4])
-#  data_new[ticker]['OBV_Low'  ] = ta.OBV(data[ticker].iloc[:,2], data[ticker].iloc[:,4])
-#  data_new[ticker]['OBV_Close'] = ta.OBV(data[ticker].iloc[:,3], data[ticker].iloc[:,4])
-#  data_open[ticker ]['EMA' ] = ta.EMA(data[ticker].iloc[:,0], timeperiod=20)
-#  data_high[ticker ]['EMA' ] = ta.EMA(data[ticker].iloc[:,1], timeperiod=20)
-#  data_low[ticker  ]['EMA' ] = ta.EMA(data[ticker].iloc[:,2], timeperiod=20)
-#  data_close[ticker]['EMA' ] = ta.EMA(data[ticker].iloc[:,3], timeperiod=20)
-#  data_new[ticker].rename(columns={data_new[ticker].columns[0]: "SMA_Open"}, inplace=True)
+
+# =============================================================================
+#   #Data all in data_new
+#   data_new[ticker]              = ta.SMA(data[ticker].iloc[:,0], timeperiod=20).to_frame()
+#   data_new[ticker]['SMA_High' ] = ta.SMA(data[ticker].iloc[:,1], timeperiod=20)
+#   data_new[ticker]['SMA_Low'  ] = ta.SMA(data[ticker].iloc[:,2], timeperiod=20)
+#   data_new[ticker]['SMA_Close'] = ta.SMA(data[ticker].iloc[:,3], timeperiod=20)
+#   data_new[ticker]['RSI_Open' ] = ta.RSI(data[ticker].iloc[:,0], timeperiod=14)
+#   data_new[ticker]['RSI_High' ] = ta.RSI(data[ticker].iloc[:,1], timeperiod=14)
+#   data_new[ticker]['RSI_Low'  ] = ta.RSI(data[ticker].iloc[:,2], timeperiod=14)
+#   data_new[ticker]['RSI_Close'] = ta.RSI(data[ticker].iloc[:,3], timeperiod=14)
+#   data_new[ticker]['OBV_Open' ] = ta.OBV(data[ticker].iloc[:,0], data[ticker].iloc[:,4])
+#   data_new[ticker]['OBV_High' ] = ta.OBV(data[ticker].iloc[:,1], data[ticker].iloc[:,4])
+#   data_new[ticker]['OBV_Low'  ] = ta.OBV(data[ticker].iloc[:,2], data[ticker].iloc[:,4])
+#   data_new[ticker]['OBV_Close'] = ta.OBV(data[ticker].iloc[:,3], data[ticker].iloc[:,4])
+#   data_open[ticker ]['EMA' ] = ta.EMA(data[ticker].iloc[:,0], timeperiod=20)
+#   data_high[ticker ]['EMA' ] = ta.EMA(data[ticker].iloc[:,1], timeperiod=20)
+#   data_low[ticker  ]['EMA' ] = ta.EMA(data[ticker].iloc[:,2], timeperiod=20)
+#   data_close[ticker]['EMA' ] = ta.EMA(data[ticker].iloc[:,3], timeperiod=20)
+#   data_new[ticker].rename(columns={data_new[ticker].columns[0]: "SMA_Open"}, inplace=True)
+# =============================================================================
 
 for ticker in data:
 #  data[ticker]{'y'}  = (data[ticker].iloc[:,0])
